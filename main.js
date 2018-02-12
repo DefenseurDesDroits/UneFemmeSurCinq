f6be4845d76db1c2d70702500319e3280a935948
Yorick <yorick.demichelis@defenseurdesdroits.fr>
2018-02-12 16:07:11 +0100

    add menu

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.warn('I\'ve become self aware.');
+
+function activateBurger() {
+  document.getElementById('menu').classList.toggle('closed');
+  document.querySelector('header .burger').classList.toggle('hide');
+  document.querySelector('#topbar').classList.toggle('hide');
+}
+
+function topBarToggle() {
+  if (window.scrollY > 70) {
+    document.getElementById('topbar').classList.remove('top');
+  } else {
+    document.getElementById('topbar').classList.add('top');
+  }
+}
+
+document.querySelector('#menu .burger.close').addEventListener('click', activateBurger);
+document.querySelector('header .burger').addEventListener('click', activateBurger);
+
+window.addEventListener('scroll', topBarToggle);
+
+var menulinks = document.querySelectorAll('#menu li a');
+[].forEach.call(menulinks, (link) => {
+  link.addEventListener('click', activateBurger);
+});
